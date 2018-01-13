a060a997a1931b2f606c4712b4246833c5e2419e
Henrik Oddershede <ho@obviux.dk>
2018-01-13 23:01:37 +0100

    New machine users get a home directory and /bin/bash per default.

--- create-machine-users.sh
+++ create-machine-users.sh
@@ -6,10 +6,12 @@
 	getent passwd $UNAME > /dev/null
 
 	if [ $? -eq 0 ]; then
-		echo "User " $UNAME " already exits"
+		echo "User " $UNAME " already exists"
 	else
 		echo "Creating new user " $UNAME
-		sudo useradd $UNAME
+                sudo mkdir -p /home/$UNAME
+                sudo useradd -d /home/$UNAME -s /bin/bash $UNAME
+                sudo chown -R $UNAME:$UNAME /home/$UNAME
 
 		# TODO: CP -> make this dependant on DynamoDB config
 		echo "Granting sudo to user"
